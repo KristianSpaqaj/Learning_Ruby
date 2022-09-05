834a8b0d47fd85517901d3c08a21fec478ea1b0b
KristianSpaqaj <kris49j7@edu.easj.dk>
2022-09-05 11:41:07 +0200

    lærte hvordan man laver funktioner og klasser

--- learning.rb
+++ learning.rb
@@ -1,1 +1,18 @@
-puts("Hello world")
+def test(a,b)
+    puts(a*b)
+end
+test(5,9)
+class Greeter
+    def initialize(name = "World")
+        @name = name
+    end
+    def say_hi
+        puts("Hi #{@name}")
+    end
+    def say_bye
+        puts("Bye #{@name}")
+    end
+end
+greeter = Greeter.new("lars")
+greeter.say_hi
+greeter.say_bye
