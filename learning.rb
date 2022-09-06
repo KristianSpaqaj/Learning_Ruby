fc246a6c319440318fa2d31f332f9ff39fa4e6d0
KristianSpaqaj <kris49j7@edu.easj.dk>
2022-09-06 16:38:23 +0200

    testing global variables and if-statements and while loops

--- learning.rb
+++ learning.rb
@@ -16,3 +16,17 @@ end
 greeter = Greeter.new("lars")
 greeter.say_hi
 greeter.say_bye
+#How to make global variables and write if statements
+$x = 55
+$f = 6
+if $x < $f
+    puts "yes"
+else
+    puts " no"
+end
+#Writing while loop
+while $f < $x
+    puts "testing #{$f}"
+    $f += 1
+end
+
