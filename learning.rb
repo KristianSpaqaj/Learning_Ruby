30d45641286df0bb44d7c67efe6c93933fcfea31
KristianSpaqaj <kris49j7@edu.easj.dk>
2022-09-13 14:19:58 +0200

    save

--- learning.rb
+++ learning.rb
@@ -3,19 +3,26 @@ def test(a,b)
 end
 test(5,9)
 class Greeter
-    def initialize(name = "World")
-        @name = name
+    def initialize(name, age)
+        @name, @age, @limit = name, age, 23
     end
     def say_hi
-        puts("Hi #{@name}")
+        puts("Hi my name is #{@name} and i am #{@age} years old")
     end
     def say_bye
-        puts("Bye #{@name}")
+        if @age > @limit
+            puts("#{@name}, you are older than the limit: #{@limit}")
+        else
+            puts("Good to flow")
+        end
     end
 end
-greeter = Greeter.new("lars")
+greeter = Greeter.new("Kristian", 24)
 greeter.say_hi
 greeter.say_bye
+greeter2 = Greeter.new("Lars", 22)
+greeter2.say_hi
+greeter2.say_bye
 #How to make global variables and write if statements
 x = 7
 f = 6
@@ -34,11 +41,11 @@ def sizeof_string_input(string)
     if string.empty?
         puts("String is empty")
     else
-        puts(string.size)
+        puts("The size of the string: \"#{string}\" is #{string.size}")
     end
 end
 #Testing inputs. Had to change to run on terminal because reading inputs otherwise does not work
 puts("Enter input")
-#$input = gets.chomp
-sizeof_string_input("+++++++++++++++++++++++++++++")
+$input = gets.chomp
+sizeof_string_input($input)
 
